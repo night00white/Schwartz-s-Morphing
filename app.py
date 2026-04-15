2401522ff2dd8e3eee90116c33711579ebeb0b97
YaluoW <63413809+night00white@users.noreply.github.com>
2026-04-15 02:31:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -97,5 +97,10 @@ def get_grid_result():
         return Response(buffer.tobytes(), mimetype='image/jpeg')
     return "Not ready", 400
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080, use_reloader=False)
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080)),
+        debug=True
+    )
